b5900417692863c113adab9d253f3ee0dd2a527c
Jack Clifton <outlaw21kawasaki@gmail.com>
2025-06-02 21:25:22 +0100

    Add databases and allow new projects to be added

--- pawprint.py
+++ pawprint.py
@@ -11,26 +11,32 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 
 
 class Privileges:
-	read_only = 0   # can read/search only
-	developer = 1   # can create review requests
-	approver = 2    # can approve requests
-	admin = 3       # can manage user accounts
+	none = 0		# default value for new accounts
+	read_only = 1	# can read/search only
+	developer = 2	# can create review tickets
+	approver = 3	# can approve tickets
+	admin = 4		# can manage user accounts
+
+
+class StatusTypes:
+	none = 0		# for new accounts
+	review = 1		# initial status for new ticket
+	corrections = 2	# reviewer has feedback for changes
+	closed = 3		# ticket is closed and not implemented
+	approved = 4	# changes can be moved back to main branches
+	confirm = 5		# changes have been moved
+	complete = 6	# reviewer confirms nothing is missing
 
 
 class Projects(db.Model):
 	project_id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(20), nullable=False)
-
-
-class StatusTypes(db.Model):
-	status_id = db.Column(db.Integer, primary_key=True)
-	label = db.Column(db.Integer, nullable=False)
+	name = db.Column(db.String(64), nullable=False, unique=True)
 
 
 class Users(db.Model):
 	user_id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), nullable=False)
-	email = db.Column(db.String(128), nullable=False)
+	email = db.Column(db.String(128), nullable=False, unique=True)
 	privileges = db.Column(db.Integer, nullable=False)
 	created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 	updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
@@ -38,11 +44,11 @@ class Users(db.Model):
 
 class Review(db.Model):
 	review_id = db.Column(db.Integer, primary_key=True)
-	project_id = db.Column(db.Integer, nullable=False, db.ForeignKey('Projects.project_id'))
+	project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'), nullable=False)
 	branch = db.Column(db.String(64), nullable=False)
 	commits = db.Column(db.Text, nullable=False)
 	ers_number = db.Column(db.Integer, nullable=False)
-	author_id = db.Column(db.Integer, nullable=False, db.ForeignKey('Users.user_id'))
+	author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 	created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
 	project = db.relationship('Projects', backref=db.backref('reviews', lazy=True))
@@ -51,15 +57,15 @@ class Review(db.Model):
 
 class Status(db.Model):
 	status_id = db.Column(db.Integer, primary_key=True)
-    review_id = db.Column(db.Integer, nullable=False, db.ForeignKey('Review.review_id'))
-    status = db.Column(db.Integer, nullable=False, db.ForeignKey('Review.review_id'))
-    actor_id = db.Column(db.Integer, nullable=False, db.ForeignKey('Users.user_id'))
-    modified_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    notes = db.Column(db.Text)
+	review_id = db.Column(db.Integer, db.ForeignKey('review.review_id'), nullable=False)
+	status = db.Column(db.Integer, nullable=False)
+	actor_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+	modified_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+	notes = db.Column(db.Text)
 
-    # Relationships
-    review = db.relationship('Review', backref=db.backref('status_history', lazy=True, cascade="all, delete-orphan"))
-    actor = db.relationship('Users', backref=db.backref('status_actions', lazy=True))
+	# Relationships
+	reviewd = db.relationship('Review', backref=db.backref('status_history', lazy=True, cascade="all, delete-orphan"))
+	actors = db.relationship('Users', backref=db.backref('status_actions', lazy=True))
 
 
 
@@ -75,10 +81,21 @@ def home():
 	return flask.render_template("home.html")
 
 
-@app.route("/jack")
-def jack():
-	items = Item.query.all()
-	return flask.render_template("jack.html", items=items)
+@app.route("/projects", methods=["GET", "POST"])
+def projects():
+
+	if flask.request.method == "POST":
+		project = flask.request.form.get("project")
+
+		if project:
+			new_project = Projects(name=project)
+			db.session.add(new_project)
+			db.session.commit()
+
+	projects = Projects.query.all()
+	return flask.render_template("projects.html", projects=projects)
+
+
 
 
 @app.route("/add-item", methods=["GET", "POST"])
